8ec93dec34c87114da762344dc047688558b338c
Ayush1208 <ayush.mishra1208@gmail.com>
2020-08-21 00:55:15 +0530

    added multiplication

--- assignment1.py
+++ assignment1.py
@@ -4,12 +4,15 @@ def addition(a,b):
 def subtraction(a,b):
 	return a-b;
 
+def mul(a,b):
+	return a*b
 
 if __name__ == "__main__":
 	a = 3
 	b = 5
 	print(addition(a,b))
 	print(subtraction(a,b))
+	print(mul(a,b))
 
 
 '''
